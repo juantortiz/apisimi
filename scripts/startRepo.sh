6b3ea5f3c9434dcad4a347cd1d9813cb1f793f27
rburdet <rburdet@localhost.localdomain>
2017-07-05 16:51:47 -0300

    cambios para correr localmente '

--- scripts/startRepo.sh
+++ scripts/startRepo.sh
@@ -1,7 +1,6 @@
-cd /var/www/html/simi/api
 . .venv/bin/activate
-python api/reportDjaiAndA1dest.py http://186.33.211.247:89/kie-wb-distribution-wars-6.5.0.Final-wildfly10 admin admin /u01/simi/simi_output 
+python api/reportDjaiAndA1dest.py http://186.33.211.247:89/kie-wb-distribution-wars-6.5.0.Final-wildfly10 admin admin /home/rburdet/miprod/simi_output 
 DESDE=$(date +%Y-%m-%d_17:00:00 -d "yesterday" ) 
 HASTA=$(date '+%Y-%m-%d_17:00:00')
-python api/parseAprobadoAndSend.py /u01/simi/scripts/config.ini $DESDE $HASTA APROBADO /u01/simi/scripts/emailsprueba.csv
-python api/parseAprobadoAndSend.py /u01/simi/scripts/config.ini $DESDE $HASTA CAUTELAR /u01/simi/scripts/emailsprueba.csv
+python api/parseAprobadoAndSend.py config.ini $DESDE $HASTA APROBADO emailsprueba.csv
+python api/parseAprobadoAndSend.py config.ini $DESDE $HASTA CAUTELAR emailsprueba.csv
