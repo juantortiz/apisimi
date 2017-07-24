a818c3ad07ea383e45a044fd74d9ecb5b8666652
rburdet <rburdet@localhost.localdomain>
2017-07-24 11:49:09 -0300

    cambio mails

--- scripts/startRepo.sh
+++ scripts/startRepo.sh
@@ -2,5 +2,5 @@
 python api/reportDjaiAndA1dest.py http://186.33.211.247:89/kie-wb-distribution-wars-6.5.0.Final-wildfly10 admin admin /home/rburdet/miprod/simi_output 
 DESDE=$(date +%Y-%m-%d_17:00:00 -d "yesterday" ) 
 HASTA=$(date '+%Y-%m-%d_17:00:00')
-python api/parseAprobadoAndSend.py config.ini $DESDE $HASTA APROBADO emailsprueba.csv
-python api/parseAprobadoAndSend.py config.ini $DESDE $HASTA CAUTELAR emailsprueba.csv
+python api/parseAprobadoAndSend.py config.ini $DESDE $HASTA APROBADA /u01/simi/scripts/emailsprueba.csv
+python api/parseAprobadoAndSend.py config.ini $DESDE $HASTA CAUTELAR /u01/simi/scripts/emailsprueba.csv
