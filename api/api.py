4bf49c2a210263deb03649b45132f826a8a55737
cesar <ccontreras.minprod@gmail.com>
2017-12-05 14:33:52 -0300

    - Se agregan campos acumulado_fob_dolares_disponible_sali acumulado_fob_dolares_disponible_sali_lna a importadores

--- api/api.py
+++ api/api.py
@@ -79,7 +79,7 @@ class ImportadorRobot(Resource):
                                   "unidad_declarada, " \
                                   "libre_acuerdo_cantidad, " \
                                   "cantidad_autorizado_vigente, " \
-                                  "fob_dolares_autorizado_vigente, " \
+                                  "fob_dolares_autorizado_vigente " \
                                "FROM acuerdo_importado_posicion " \
                                "WHERE cuit = %s ";
 
