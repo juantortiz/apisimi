fc71fe524e3485ad8d40fd92326aea35fa9a2767
nachodz <dezanignacio@gmail.com>
2017-12-14 11:47:46 -0300

    Se modifica query para que aborte todas las task activas asi no tengan id de simi asociada.

--- abortarSimis/abortSimis.py
+++ abortarSimis/abortSimis.py
@@ -12,8 +12,8 @@ deploymentID = 'minprod:simi:3.04'
 
 dbJbpm = MySQLdb.connect(host=dbhost, user=dbuser, passwd=dbpass, db=db)
 cursorJbpm = dbJbpm.cursor()
-query = "SELECT vil.processinstanceid FROM ProcessInstanceLog AS pil JOIN VariableInstanceLog AS vil ON vil.processinstanceid = pil.processinstanceid AND vil.variableId='djai_id_simi' WHERE pil.STATUS = 1;"
-# query = "SELECT pil.processinstanceid FROM ProcessInstanceLog AS pil WHERE pil.STATUS = 1";
+# query = "SELECT vil.processinstanceid FROM ProcessInstanceLog AS pil JOIN VariableInstanceLog AS vil ON vil.processinstanceid = pil.processinstanceid AND vil.variableId='djai_id_simi' WHERE pil.STATUS = 1;"
+query = "SELECT pil.processinstanceid FROM ProcessInstanceLog AS pil WHERE pil.STATUS = 1";
 cursorJbpm.execute(query)
 process = cursorJbpm.fetchall();
 
